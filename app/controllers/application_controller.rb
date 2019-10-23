831c453b659301e4835ee333c739f26b04e1b5ed
Ubuntu <ubuntu@ip-172-31-81-129.ec2.internal>
2019-10-23 16:56:15 +0000

    update user profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,9 @@ class ApplicationController < ActionController::Base
 
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+         devise_parameter_sanitizer.permit(:account_update, keys: [:username,:address,:phone,:avatar])
+    end
+    def after_update_path_for(resource)
+      user_path(resource)
     end
 end
