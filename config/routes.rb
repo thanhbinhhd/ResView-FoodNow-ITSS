5d4cdfa40586eabfdfd65b415f8cc3cc807c3408
Hang Pham <hanghedspibk@gmail.com>
2019-11-05 00:01:00 +0700

    Update user profile: return to edit page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  devise_for :users
+
+  # devise_for :users
+  devise_for :users, :controllers => { :registrations => :registrations }
 end
