f246ceeb5854986ed4dd398dd391ed92aac504df
Thanh Binh <binh97.lt@gmail.com>
2019-11-11 10:49:20 +0700

    comment and vote star

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,11 @@
+class CommentsController < ApplicationController
+  def create
+    @comment = current_user.comments.build(restaurant_id: params[:comment][:restaurant_id],
+      content: params[:comment][:content])
+    @comment.save!
+    @vote = current_user.votes.build(restaurant_id: params[:comment][:restaurant_id],
+      star_num: params[:comment][:rating_input].to_f)
+    @vote.save!
+    redirect_to request.referrer || root_path
+  end
+end
