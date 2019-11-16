ced1bf56c8318bf609fab8111a1ec0008e06e9ae
Thanh Binh <binh97.lt@gmail.com>
2019-11-16 23:31:23 +0700

    add adminlte and create new transaction

---
+++ app/controllers/transactions_controller.rb
@@ -0,0 +1,14 @@
+class TransactionsController < ApplicationController
+  layout 'admin_application', only: [:new]
+
+  def new
+    @restaurants = Restaurant.all
+  end
+
+  def create
+    @tran = Restaurant.find_by(id: params[:transaction][:restaurant_id]).transactions.build
+    @tran.price = params[:transaction][:price].to_f
+    @tran.save!
+    # redirect_to admin_transaction_list_path
+  end
+end
