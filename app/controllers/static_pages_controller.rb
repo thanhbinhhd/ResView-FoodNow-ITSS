145d07b2499b6a3099f0c7f3edc57edf9c701e21
Thanh Binh <binh97.lt@gmail.com>
2019-11-27 10:14:25 +0700

    seeding data

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
-    @recent_res = Restaurant.recent.limit(6)
-    @best_res = Restaurant.best.limit(6)
+    @recent_res = Restaurant.has_deadline.recent.limit(6)
+    @best_res = Restaurant.has_deadline.best.limit(6)
   end
   def about
   end
   def contact
-  end  
+  end
 end
